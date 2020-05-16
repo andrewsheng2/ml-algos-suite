56c4909f9ad6c94955f227c7a735247a21542f4d
Andrew Sheng <44681431+andrewsheng2@users.noreply.github.com>
2020-05-16 00:20:41 -0400

    Add comments and clear function to declutter UI

--- compare_algorithms.py
+++ compare_algorithms.py
@@ -1,15 +1,18 @@
 import numpy as np
-import sys
 import time
 
+import sys
+import os
+
 from linear_regression import linear_regression
-from logistic_regression import logistic_gradient
-from logistic_regression import logistic_newtons
 # from kernel_regression import kernel_regression
 
-# from decision_tree import learn_decision_tree
-# from kmeans import kmeans
+from logistic_regression import logistic_gradient
+from logistic_regression import logistic_newtons
 
+# from decision_tree import learn_decision_tree
+
+# from kmeans import kmeans
 # from gmm import learn_gmm
 # from neural_network import learn_nerual_network
 # from recommender import learn_recommender
@@ -31,6 +34,9 @@ def print_linear_datasets():
 def print_logistic_datasets():
     f = open("texts/logistic_datasets.txt")
     print(f.read())
+
+def clear():
+    os.system('cls' if os.name=='nt' else 'clear')
 
 #######################
 #### LOAD DATASETS ####
@@ -82,12 +88,13 @@ def load_ratings():
 #### REQUESTS TO USER ####
 ##########################
 
-def request_algorithm():
+def request_algorithm(num_algs):
+    # ask user to input integer value of algorithm selection
     alg = None
     while alg == None:
         alg_input = input("Which algorithm do you want to run? Type 'exit' to close. ")
         try:
-            if int(alg_input) in range(1,16): alg = int(alg_input)
+            if int(alg_input) in range(1,num_algs+1): alg = int(alg_input)
             else: print("Error: Not a valid selection")
         except:
             if alg_input == "exit": sys.exit()
@@ -95,6 +102,7 @@ def request_algorithm():
     return alg
 
 def request_dataset(num_sets):
+    # ask user to input integer value of dataset selection
     dataset = None
     while dataset == None:
         set_input = input("Which dataset do you want to use? ")
@@ -106,6 +114,10 @@ def request_dataset(num_sets):
     return dataset
 
 def request_hyperparameter(name):
+    # ask user to input positive float value for specified hyperparameter
+    # possible hyperparameters: lambda for regularization, learning rate
+    #          for gradient descent, number of iterations for gradient descent
+    #          and newton's method`
     hyp = None
     while hyp == None:
         hyp_input = input("Please provide a positive value for "+name+": ")
@@ -120,19 +132,20 @@ def request_hyperparameter(name):
     return hyp
 
 def request_visualization():
+    # ask user whether they want to visualize dataset, returns boolean
     while True:
         vis_input = input("Do you want to visualize the trained model? (Y/n) ")
         if vis_input.lower() == "y" or vis_input.lower() == "yes": return True
         elif vis_input.lower() == "n" or vis_input.lower() == "no": return False
 
 def return_main():
+    # ask user whether they want to return to main menu or exit program, doesn't return value
     req = None
     while req == None:
         req = input("Do you want to 'return' to the main menu or 'exit'? ")
-        if req == "return":
-            print("\n")
+        if req == "return" or req == "r":
             run()
-        elif req == "exit":
+        elif req == "exit" or req == "e":
             sys.exit()
         else:
             req = None
@@ -142,20 +155,27 @@ def return_main():
 ########################
 
 def run_linear(reg):
-    print("\nSelected Linear Regression", end="")
+    print("Selected Linear Regression", end="")
     if reg: print(" with L2 Regularization.")
     else: print(".")
     print_linear_datasets()
 
+    # load requested dataset
     dataset = request_dataset(2)
     if dataset == 1: X_train, y_train, X_val, y_val = load_1D_regression()
     if dataset == 2: X_train, y_train, X_val, y_val = load_MNIST()
+
+    # request hyperparameters
     lmbd = request_hyperparameter("lambda") if reg else 0
+
+    # start training and time
     print("Starting training...")
     start_time = time.time()
     (w, mse_train, mse_val) = linear_regression(X_train, y_train, X_val, y_val, lmbd)
     end_time = time.time()
     total = round(end_time-start_time, 4)
+
+    # display results and request for visualization
     print("Finished training in", total, "seconds.")
     print("Training MSE: ", mse_train)
     print("Validation MSE: ", mse_val)
@@ -165,17 +185,22 @@ def run_linear(reg):
     return_main()
 
 def run_logistic(mode, reg):
-    print("\nSelected Linear Regression", end="")
+    print("Selected Linear Regression", end="")
     print(" using "+mode, end="")
     if reg: print(" with L2 Regularization.")
     else: print(".")
     print_logistic_datasets()
 
+    # load requested dataset
     dataset = request_dataset(1)
     if dataset == 1: X_train, y_train, X_val, y_val = load_1D_classification()
+
+    # request hyperparameters
     lmbd = request_hyperparameter("lambda") if reg else 0
     alpha = request_hyperparameter("learning rate") if mode == "Gradient Descent" else 0
     num_iter = int(request_hyperparameter("number of iterations"))
+
+    # start training and time
     print("Starting training...")
     start_time = time.time()
     if mode == "Gradient Descent":
@@ -184,6 +209,8 @@ def run_logistic(mode, reg):
         (w, obj_train, obj_val) = logistic_newtons(X_train, y_train, X_val, y_val, lmbd, num_iter)
     end_time = time.time()
     total = round(end_time-start_time, 4)
+
+    # display results and request for visualization
     print("Finished training in", total, "seconds.")
     print("Training Objective: ", obj_train)
     print("Validation Objective: ", obj_val)
@@ -192,16 +219,15 @@ def run_logistic(mode, reg):
         util.graph_logistic(w, X_train, y_train, X_val, y_val, lmbd)
     return_main()
 
-def run_kernel():
-    pass
-
 ###########################
 #### MAIN RUN FUNCTION ####
 ###########################
 
 def run():
+    clear()
     print_welcome()
-    alg = request_algorithm()
+    alg = request_algorithm(10)
+    clear()
     if alg == 1: run_linear(False)
     if alg == 2: run_linear(True)
     if alg == 7: run_logistic("Gradient Descent", False)
